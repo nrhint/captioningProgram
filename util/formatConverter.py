9615fa64391803a0b45595b09d9ecaa00934dc64
nrhint <nathan@hintonclan.org>
2022-03-07 21:22:49 -0800

    fixed a minor flaw that made Kdenlive reject the converted files in some cases

--- util/formatConverter.py
+++ util/formatConverter.py
@@ -18,17 +18,28 @@ class Convert:
             index +=1
         lines = lines[index:]
         counter = 1
+        lastNewLine = False
         for line in lines:
-            print(line)
-            if line == '': #If it was a new line:
+            #print(line)
+            if line == '' and lastNewLine == False: #If it was a new line and there was not a new line before it:
+                pass
                 self.output += '\n'
-            elif line[2] == ':' and (line[5] == '.' or line[5] == ':'):
+                lastNewLine = True
+            elif line == '' and lastNewLine == True: #Do not add anything because there is already a gap
+                pass
+            ##This section needs to be improved. Some files are done with hh:mm:ss.mmm which results in a line length of 29 while others are mm:ss.mmm which results in a line length of 23
+            elif '-->' in line: #Add the line number and timestamp
                 self.output += '%s\n'%counter
-                self.output += line[:23]+'\n' #Used to be 29
+                if line[5] == '.': #timestamp format is mm:ss.mmm
+                    self.output += '00:'+line[0:14]+'00:'+line[14:23]+'\n' #Used to be 29
+                else: #timestamp format is hh:mm:ss.mmm
+                    self.output += line[:29]+'\n'
                 counter += 1
+                lastNewLine = False
             else:
-                self.output += line + '\n'
-            # Recurse up the filepath as to generate a new filename
+                self.output += line + ' \n'
+                lastNewLine = False
+        # Recurse up the filepath as to generate a new filename
         for x in range(len(filename), 0, -1):
             if filename[0:x][-1] == '/':
                 path = filename[0:x-1]
